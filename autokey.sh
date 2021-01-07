ea3116768ae896d9b4d1c7a25f728d927d89c257
sunke <sunke@elibot.cn>
2021-01-07 16:28:15 +0800

    task-dev support autokey

--- autokey.sh
+++ autokey.sh
@@ -4,6 +4,6 @@
 cfg0=`cat /sys/fsl_otp/HW_OCOTP_CFG0`
 cfg1=`cat /sys/fsl_otp/HW_OCOTP_CFG1`
 
-md5res=`echo "root"${cfg0:2}${cfg1:2}"elite" | md5sum`
+md5res=`echo "root"${cfg0:2}${cfg1:2} | md5sum`
 
 echo "root:"${md5res:0:8} | chapasswd
